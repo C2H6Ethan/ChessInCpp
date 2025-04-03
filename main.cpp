ab5f92fcf6ae5d38d149683d2d1383392651798a
C2H6Ethan <ejbaum@live.com>
2025-04-04 00:52:40 +0200

    added example move

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <cstdint>
 #include <iostream>
-typedef unsigned long long U64;
 
-enum SquareIndex : int {
+// Type Aliases
+typedef uint64_t Bitboard;
+
+// Enums
+enum Square : int {
     a1, b1, c1, d1, e1, f1, g1, h1,
     a2, b2, c2, d2, e2, f2, g2, h2,
     a3, b3, c3, d3, e3, f3, g3, h3,
@@ -12,12 +16,13 @@ enum SquareIndex : int {
     a8, b8, c8, d8, e8, f8, g8, h8
 };
 
-constexpr U64 bitboard_from_square(SquareIndex s) {
+// Board Utilities
+constexpr Bitboard square_to_bitboard(Square s) {
     // gets bitboard with square set to 1
     return 1ULL << s; //shifts 0000000001 s places so if s is 3 (d1 in Square enum) then 0001 -> 1000
 }
 
-void print_board(U64 bitboard) {
+void print_bitboard(Bitboard bitboard) {
     for (int rank = 7; rank >= 0; rank--) {
         std::cout << rank + 1 << " ";
         for (int file = 0; file < 8; file++) {
@@ -31,25 +36,35 @@ void print_board(U64 bitboard) {
 
 }
 
+// Piece Bitboards
+Bitboard white_pawns   = 0xFF00ULL;
+Bitboard black_pawns   = 0x00FF000000000000ULL;
+Bitboard white_knights = square_to_bitboard(b1) | square_to_bitboard(g1);
+Bitboard black_knights = square_to_bitboard(b8) | square_to_bitboard(g8);
+Bitboard white_rooks   = square_to_bitboard(a1) | square_to_bitboard(h1);
+Bitboard black_rooks   = square_to_bitboard(a8) | square_to_bitboard(h8);
+Bitboard white_bishops = square_to_bitboard(c1) | square_to_bitboard(f1);
+Bitboard black_bishops = square_to_bitboard(c8) | square_to_bitboard(f8);
+Bitboard white_queen   = square_to_bitboard(d1);
+Bitboard black_queen   = square_to_bitboard(d8);
+Bitboard white_king    = square_to_bitboard(e1);
+Bitboard black_king    = square_to_bitboard(e8);
 
+// --- Move Logic ---
+void make_move(Bitboard& piece_bb, Square from, Square to) {
+    piece_bb &= ~square_to_bitboard(from); // Clear 'from'
+    piece_bb |= square_to_bitboard(to);    // Set 'to'
+}
 
 int main() {
-    U64 white_pawns = 0xFF00ULL;
-    U64 black_pawns = 0x00FF000000000000ULL;
-    U64 white_knights = bitboard_from_square(b1) | bitboard_from_square(g1);
-    U64 black_knights = bitboard_from_square(b8) | bitboard_from_square(g8);
-    U64 white_rooks = bitboard_from_square(a1) | bitboard_from_square(h1);
-    U64 black_rooks = bitboard_from_square(a8) | bitboard_from_square(h8);
-    U64 white_bishops = bitboard_from_square(c1) | bitboard_from_square(f1);
-    U64 black_bishops = bitboard_from_square(c8) | bitboard_from_square(f8);
-    U64 white_queen = bitboard_from_square(d1);
-    U64 black_queen = bitboard_from_square(d8);
-    U64 white_king = bitboard_from_square(e1);
-    U64 black_king = bitboard_from_square(e8);
+    Bitboard all_pieces = white_pawns | black_pawns | white_knights | white_rooks | black_knights | black_rooks | white_bishops | black_bishops | white_queen | black_queen | white_king | black_king;
+    print_bitboard(all_pieces);
 
-    U64 all_pieces = white_pawns | black_pawns | white_knights | white_rooks | black_knights | black_rooks | white_bishops | black_bishops | white_queen | black_queen | white_king | black_king;
-
-    print_board(all_pieces);
+    // Example: Move white pawn from A2 to A4
+    make_move(white_pawns, a2, a4);
+    all_pieces = white_pawns | black_pawns | white_knights | white_rooks | black_knights | black_rooks | white_bishops | black_bishops | white_queen | black_queen | white_king | black_king;
+    std::cout << "\nAfter moving pawn A2->A4:\n";
+    print_bitboard(all_pieces);
 
     return 0;
 }
